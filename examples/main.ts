4b7d8e06eab11439ef34dc8f079df6a8e51733b0
宁建浩 <375332835@qq.com>
2023-03-02 11:15:00 +0800

    项目初始化

---
+++ examples/main.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: 宁建浩 375332835@qq.com
+ * @Date: 2023-03-02 10:11:26
+ * @LastEditors: 宁建浩 375332835@qq.com
+ * @LastEditTime: 2023-03-02 10:11:32
+ * @FilePath: \asd-ui\examples\main.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+import { createApp } from 'vue';
+import App from './app.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
---
+++ examples/vite.config.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: 宁建浩 375332835@qq.com
+ * @Date: 2023-03-02 10:09:38
+ * @LastEditors: 宁建浩 375332835@qq.com
+ * @LastEditTime: 2023-03-02 10:09:50
+ * @FilePath: \asd-ui\examples\vite.config.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+});
